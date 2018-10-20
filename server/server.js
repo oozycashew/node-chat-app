13b9920b8ae4c6fffdebbbdd698a221d52a45682
Ray Go <raymondjgo@gmail.com>
2018-10-19 22:55:39 -0700

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (newMessage, callback) => {
     console.log('createMessage', newMessage);
     io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
-    callback('This is from the server.');
+    callback();
   });
 
   socket.on('disconnect', () => {
